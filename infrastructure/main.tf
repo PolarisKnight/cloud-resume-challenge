95912de1ce977540c20ecbc89d66dd6435311925
Braydon Tiffany <braydon.tiffany@gmail.com>
2026-08-12 15:25:58 -0600

    added backend s3 configutation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,11 +2,26 @@
 # In addition to this being used for the CRC, this will also serve as somehting as "boilerplate" reference for creating real IaC code with Terraform.
 
 # the AWS provider will automatically see the access key and secret key vars from the workflow and apply
+
+# The Terraform block is used for configuring Terraform behavior itself.
+terraform {
+  backend "s3" {
+    bucket       = "tfstate-gh"
+    key          = "prod/terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = "true"
+    use_lockfile = "true" # Requires Terraform 1.11+ 
+
+  }
+}
+
+
 provider "aws" {
   region = "us-west-2"
 }
 
 resource "aws_s3_bucket" "example" {
     bucket = "my-example-bucket-braydon"
+
   
 }
