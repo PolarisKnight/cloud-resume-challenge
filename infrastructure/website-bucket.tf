8696e419c96ae81dd4a26943d84a8954c96262d0
Braydon Tiffany <braydon.tiffany@gmail.com>
2026-09-11 22:49:16 -0600

    added dns/cdn/certificate resources

--- infrastructure/website-bucket.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "website_s3_bucket" {
-    bucket = "braydon-cloud-resume-website"
-
-    tags = {
-        Name = "braydon-cloud-resume-website"
-        ManagedBy = "Terraform"
-        Enviroment = "Prod"
-     }
-}
